f96a6bbbf1a4c78b19efc2cf5a624a7e9a9c5a3c
edwin magairwa <edwinmagairwa@gmail.com>
2025-05-19 15:46:06 +0300

    added shop

--- customer.py
+++ customer.py
@@ -9,9 +9,15 @@ class Customer:
     def name(self):
         return self._name
 
+    def coffees(self):
+        return list(set(order.coffee for order in self.orders()))
+
+    def create_order(self, coffee, price):
+        if not isinstance(coffee, Coffee):
+
     @name.setter
     def name(self, value):
-        if not isinstance(value, str) or not (1 <= len(value) <= 15):
+       if  not isinstance(value, str) or not (1 <= len(value) <= 15):
             raise ValueError("Name must be a string between 1 and 15 characters")
         self._name = value
 
