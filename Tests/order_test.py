1e69df5d2df1bc80fc3562dd4a7d36178a479c99
edwin magairwa <edwinmagairwa@gmail.com>
2025-05-19 15:30:03 +0300

    added challenge

--- Tests/order_test.py
+++ Tests/order_test.py
@@ -12,13 +12,14 @@ class TestOrder(unittest.TestCase):
             Order(self.customer, self.coffee, 0.5)
         with self.assertRaises(ValueError):
             Order(self.customer, self.coffee, 11.0)
-    
+    with self.assertRaises(ValueError):
+            Order(self.customer, self.coffee, "5.0")
+
+    def test_immutable_price(self):
+        order = Order(self.customer, self.coffee, 5.0)
+        with self.assertRaises(AttributeError):
             order.price = 6.0
 
     def test_customer_and_coffee(self):
         order = Order(self.customer, self.coffee, 5.0)
-        self.assertEqual(order.customer, self.customer)
-        self.assertEqual(order.coffee, self.coffee)
-
-if __name__ == '__main__':
-    unittest.main()
+        
