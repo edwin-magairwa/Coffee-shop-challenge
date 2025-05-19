83e0c9f1856a011fc0b76f8d6ffa921040a5c970
edwin magairwa <edwinmagairwa@gmail.com>
2025-05-19 15:24:13 +0300

    added challenge

--- Tests/order_test.py
+++ Tests/order_test.py
@@ -2,13 +2,12 @@ import unittest
 from customer import Customer
 from coffee import Coffee
 from order import Order
-
-    def test_price_validation(self):
-        with self.assertRaises(ValueError):
-            Order(self.customer, self.coffee, 0.5)
-        with self.assertRaises(ValueError):
-            Order(self.customer, self.coffee, 11.0)
-        with self.assertRaises(ValueError):
+class TestOrder(unittest.TestCase):
+    def setUp(self):
+        Order.all_orders = []
+        self.customer = Customer("Alice")
+        self.coffee = Coffee("Espresso")
+    ith self.assertRaises(ValueError):
             Order(self.customer, self.coffee, "5.0")
 
     def test_immutable_price(self):
